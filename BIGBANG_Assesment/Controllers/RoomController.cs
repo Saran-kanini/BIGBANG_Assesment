12975d0bfdaf9cf25d8d614e48dc92022b73a794
Saran-kanini <saran.kanini@gmail.com>
2023-05-28 18:06:16 +0530

    Commit5

--- BIGBANG_Assesment/Controllers/RoomController.cs
+++ BIGBANG_Assesment/Controllers/RoomController.cs
@@ -1,10 +1,12 @@
 ﻿using BIGBANG_Assesment.Models;
 using BIGBANG_Assesment.Repository;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BIGBANG_Assesment.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class RoomController : ControllerBase
