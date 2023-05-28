6daec9969a91c5454c54484ccf978f3eca33cacd
Saran-kanini <saran.kanini@gmail.com>
2023-05-28 17:26:44 +0530

    Commit3

---
+++ BIGBANG_Assesment/Controllers/ReservationsController.cs
@@ -0,0 +1,70 @@
+﻿using BIGBANG_Assesment.Models;
+using BIGBANG_Assesment.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BIGBANG_Assesment.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly IReservationRepository _reservationRepository;
+
+        public ReservationsController(IReservationRepository reservationRepository)
+        {
+            _reservationRepository = reservationRepository;
+        }
+
+        // GET: api/Reservations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Reservation>>> GetReservations()
+        {
+            var reservations = await _reservationRepository.GetReservations();
+            return Ok(reservations);
+        }
+
+        // GET: api/Reservations/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Reservation>> GetReservation(int id)
+        {
+            var reservation = await _reservationRepository.GetReservation(id);
+            if (reservation == null)
+                return NotFound();
+
+            return Ok(reservation);
+        }
+
+        // PUT: api/Reservations/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReservation(int id, Reservation reservation)
+        {
+            var result = await _reservationRepository.UpdateReservation(id, reservation);
+            if (!result)
+                return NotFound();
+
+            return NoContent();
+        }
+
+        // POST: api/Reservations
+        [HttpPost]
+        public async Task<ActionResult<Reservation>> PostReservation(Reservation reservation)
+        {
+            var createdReservation = await _reservationRepository.CreateReservation(reservation);
+            return CreatedAtAction(nameof(GetReservation), new { id = createdReservation.Reservation_Id }, createdReservation);
+        }
+
+        // DELETE: api/Reservations/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteReservation(int id)
+        {
+            var result = await _reservationRepository.DeleteReservation(id);
+            if (!result)
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
